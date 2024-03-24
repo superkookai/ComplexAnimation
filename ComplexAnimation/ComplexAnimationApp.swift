9d8db6252bed074dc740f3ea160fb28ebe005733
Weerawut Chaiyasomboon <weerawut@live.com>
2024-03-24 13:00:52 +0700

    Initial Commit

---
+++ ComplexAnimation/ComplexAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexAnimationApp.swift
+//  ComplexAnimation
+//
+//  Created by Weerawut Chaiyasomboon on 24/3/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct ComplexAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
